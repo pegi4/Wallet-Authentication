7b7657a286b65f4420d26741d4aad175a17b7633
Vito Senič <vito.senic@gmail.com>
2023-05-16 09:44:52 +0200

    sign in "cancelled"

--- frontend/src/signin.jsx
+++ frontend/src/signin.jsx
@@ -9,39 +9,53 @@ export default function SignIn({ onLogin }) {
   const { signMessageAsync } = useSignMessage();
 
   const handleAuth = async () => {
+    // disconnects the web3 provider if it's already active
     if (isConnected) {
       await disconnectAsync();
     }
 
-    const { account, chain } = await connectAsync({
-      connector: new InjectedConnector(),
-    });
+    try {
+      // enabling the web3 provider metamask
+      const { account, chain } = await connectAsync({
+        connector: new InjectedConnector(),
+      });
+      
+      const userData = { address: account, chain: chain.id, network: 'evm' };
 
-    const userData = { address: account, chain: chain.id, network: 'evm' };
+      const { data } = await axios.post(
+        // making a post request to our 'request-message' endpoint
+        `${process.env.REACT_APP_SERVER_URL}/request-message`,
+        userData,
+        {
+          headers: {
+            'content-type': 'application/json',
+          },
+        }
+      );
+      const message = data.message;
+      // signing the received message via metamask
+      const signature = await signMessageAsync({ message });
 
-    const { data } = await axios.post(
-      `${process.env.REACT_APP_SERVER_URL}/request-message`,
-      userData,
-      {
-        headers: {
-          'content-type': 'application/json',
-        },
+      // if user cancelled the signature request, signature will be undefined or null
+      if (!signature) {
+        console.log("User cancelled signature request");
+        return;  // return from the function immediately
       }
-    );
-    const message = data.message;
 
-    const signature = await signMessageAsync({ message });
+      await axios.post(
+        `${process.env.REACT_APP_SERVER_URL}/verify`,
+        {
+          message,
+          signature,
+        },
+        { withCredentials: true } // set cookie from Express server
+      ).then(({ data }) => {
+        onLogin(data);
+      });
 
-    await axios.post(
-      `${process.env.REACT_APP_SERVER_URL}/verify`,
-      {
-        message,
-        signature,
-      },
-      { withCredentials: true }
-    ).then(({ data }) => {
-      onLogin(data);
-    });
+    } catch (err) {
+      console.error(err);
+    }
   };
 
   return (
